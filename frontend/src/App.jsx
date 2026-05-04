4ec93400ba86b82bbb5eff9a074c185d4717c4d7
Vikash Kumar <vikashtech0089@gmail.com>
2026-05-04 06:08:40 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
